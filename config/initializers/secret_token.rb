5e4fc9da99d27ec58a6eda2f420089b2a2871ff0
Jeffrey Hu <hujh14@episcopalacademy.org>
2014-04-06 01:02:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PizzaFinder::Application.config.secret_key_base = '071d5563a073b23d8c36e581e7be8416a8654996ad8cb7964f2a2d55c7586a20eccb6c70ebb7ba40d7d229020d17f73b10ea4b7e658d754346d6e0946325680c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PizzaFinder::Application.config.session_store :cookie_store, key: '_PizzaFinder_session'
